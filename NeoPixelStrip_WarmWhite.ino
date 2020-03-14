39ae151730adf07013def4cb8e67e8ca947d9c7b
Stephen Gilliland <stephenmatsuta@gmail.com>
2020-03-14 13:04:00 -0400

    Add files via upload

---
+++ NeoPixelStrip_WarmWhite.ino
@@ -0,0 +1,34 @@
+#include <Adafruit_NeoPixel.h>
+#ifdef __AVR__
+  #include <avr/power.h>
+#endif
+
+#define PIN 6
+
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(60, PIN, NEO_GRB + NEO_KHZ800);
+
+void setup() {
+  // This is for Trinket 5V 16MHz, you can remove these three lines if you are not using a Trinket
+  #if defined (__AVR_ATtiny85__)
+    if (F_CPU == 16000000) clock_prescale_set(clock_div_1);
+  #endif
+  // End of trinket special code
+
+
+  strip.begin();
+  strip.show(); // Initialize all pixels to 'off'
+}
+
+void loop() {
+  // Some example procedures showing how to display to the pixels:
+  colorWipe(strip.Color(255, 70, 10), 50); // Red
+}
+
+// Fill the dots one after the other with a color
+void colorWipe(uint32_t c, uint8_t wait) {
+  for(uint16_t i=0; i<8; i++) {
+    strip.setPixelColor(i, c);
+    strip.show();
+    delay(300);
+  }
+}
